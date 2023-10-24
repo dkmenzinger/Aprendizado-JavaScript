a12699aa6a394bb10a3254ee39e18cf13d235e76
dkmenzinger <137235293+dkmenzinger@users.noreply.github.com>
2023-10-24 02:41:40 -0300

    Update_03
    Material estudo dia 24/10/23

---
+++ 23-10-2023/function_01.js
@@ -0,0 +1,12 @@
+let x = 1224;
+let y = 1565;
+let z = 22;
+
+let resultado;
+
+function matematica () {
+    resultado = (x + y) * z; 
+    console.log(resultado);
+}
+
+matematica();
---
+++ 23-10-2023/function_04.js
@@ -0,0 +1,13 @@
+
+
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrecimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrecimo
+}
+
+console.log(incrementarJuros(100,10));
+// 10%.
+console.log(incrementarJuros(100,15));
+// 15%.
+console.log(incrementarJuros(100,20));
+// 20%.
